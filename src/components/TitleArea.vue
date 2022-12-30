d35141bfd4c6ac4c3908f960e07c04176c178cb3
George Tiersma <gtiersma@juno.com>
2022-12-30 11:06:57 -0800

    Built Title Page
    Created the title page portion.
    
    Also:
    
    - Corrected dependencies
    - Created Readme
    - Removed conflicting styles from main.css

---
+++ src/components/TitleArea.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>George Tiersma - Junior Fullstack Software Engineer</h1>
+
+  <button class="btn btn-primary">View Resume</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({})
+</script>
+
+<style scoped>
+
+</style>
